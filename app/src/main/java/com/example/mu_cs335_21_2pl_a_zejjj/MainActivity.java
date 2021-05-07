1ee15dc5f6b770d35aa43e1ee50b5e8e632b4bd2
zaid aljarrah <zaidjarrah2002@gmail.com>
2021-05-07 05:42:37 +0100

    ui changes

--- app/src/main/java/com/example/mu_cs335_21_2pl_a_zejjj/MainActivity.java
+++ app/src/main/java/com/example/mu_cs335_21_2pl_a_zejjj/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_notifications, R.id.navigation_home, R.id.navigation_account)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
