a7a785dad661942730079feff6b45e38d8f4e26e
Gallinagh <eoingallinagh@gmail.com>
2021-05-07 18:28:31 +0100

    Messages to all contacts

--- app/src/main/java/com/example/mu_cs335_21_2pl_a_zejjj/MainActivity.java
+++ app/src/main/java/com/example/mu_cs335_21_2pl_a_zejjj/MainActivity.java
@@ -29,8 +29,31 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.Manifest.*;
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0;
+    EditText etPhone;
+    Button btSend;
+
+    GPSTrack gps;
+
+    double latitude;
+    double longitude;
 
 
     @Override
@@ -38,6 +61,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //Assign Variable
+        btSend = findViewById(R.id.emergency_button);
+
+        gps = new GPSTrack(this);
+
+
+        latitude = gps.getLatitude();
+        longitude = gps.getLongitude();
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            //When permission is granted
+            //Create Method
+
+            gps.getLocation();
+        }
+        else{
+            //When permission is not granted
+            //Request Permission
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission.ACCESS_FINE_LOCATION}, 100);
+        }
+
+
+
+
+
+
+
+
+
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Check condition
+                if(ContextCompat.checkSelfPermission(MainActivity.this, permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+                    //When permission is granted
+                    //Create Method
+
+                    sendMessage();
+                }
+                else{
+                    //When permission is not granted
+                    //Request Permission
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, 100);
+                }
+
+            }
+
+
+        });
 
 
         BottomNavigationView navView = findViewById(R.id.nav_home);
@@ -98,4 +171,51 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void sendMessage(){
+        gps.getLocation();
+        latitude = gps.getLatitude();
+        longitude = gps.getLongitude();
+
+        DBManager db = new DBManager(FirebaseFirestore.getInstance(), "users");
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DocumentReference document = db.db.collection("users").document(uid);
+        document.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot snap_document = task.getResult();
+                    if (snap_document.exists()) {
+                        Map<String, Object> data = snap_document.getData();
+                        List<String> contacts = (List<String>)data.get("contacts");
+                        for (int i = 0; i < contacts.size(); i++) {
+                            String phone = contacts.get(i).trim();
+
+                            String sMessage = "Testing, My Location is: http://maps.google.com/?q=" + latitude + "," + longitude;
+
+                            //Check Condition
+                            if (!phone.equals("")) {
+                                //When both edit text value so not equal to blank
+                                //Initialise sms manager
+                                SmsManager smsManager = SmsManager.getDefault();
+
+
+                                if (gps.canGetLocation() == true) {
+                                    longitude = gps.getLongitude();
+                                    latitude = gps.getLatitude();
+
+                                    //Send text
+                                    smsManager.sendTextMessage(phone, null, sMessage, null, null);
+                                    Toast.makeText(getApplicationContext(), "SMS Sent Successfully", Toast.LENGTH_LONG).show();
+                                }
+                            } else {
+                                //When edit text value is blank
+                                //Display toast
+                                Toast.makeText(getApplicationContext(), "Enter Value First", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
 }   
--- app/src/main/java/com/example/mu_cs335_21_2pl_a_zejjj/fragments/HomeFragment.java
+++ app/src/main/java/com/example/mu_cs335_21_2pl_a_zejjj/fragments/HomeFragment.java
@@ -20,13 +20,6 @@ import com.example.mu_cs335_21_2pl_a_zejjj.R;
 public class HomeFragment extends Fragment {
 
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS =0;
-    EditText etPhone;
-    Button btSend;
-
-    GPSTrack gps;
-
-    double latitude;
-    double longitude;
 
     public HomeFragment() {
         super(R.layout.fragment_home);
@@ -34,76 +27,5 @@ public class HomeFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        etPhone = view.findViewById(R.id.enter_Phone);
-        btSend = view.findViewById(R.id.emergency_button);
-
-        gps = new GPSTrack(this.getView().getContext());
-
-
-
-        latitude = gps.getLatitude();
-        longitude = gps.getLongitude();
-
-        //PERMS
-        if(ContextCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            //When permission is granted
-            //Create Method
-
-            gps.getLocation();
-        }
-        else{
-            //When permission is not granted
-            //Request Permission
-            ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 100);
-        }
     }
-
-    public void emergencyButton(View v) {
-        //Check condition
-        if(ContextCompat.checkSelfPermission(v.getContext(), Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
-            //When permission is granted
-            //Create Method
-
-            sendMessage();
-        }
-        else{
-            //When permission is not granted
-            //Request Permission
-            ActivityCompat.requestPermissions(this.getActivity(), new String[]{Manifest.permission.SEND_SMS}, 100);
-        }
-    }
-
-    private void sendMessage(){
-        gps.getLocation();
-        latitude = gps.getLatitude();
-        longitude = gps.getLongitude();
-        //Get values from edit text
-        String sPhone = etPhone.getText().toString().trim();
-        String sMessage = "Testing, My Location is: http://maps.google.com/?q="+ latitude +","+longitude;
-
-        //Check Condition
-        if (!sPhone.equals("")){
-            //When both edit text value so not equal to blank
-            //Initialise sms manager
-            SmsManager smsManager = SmsManager.getDefault();
-
-
-
-            if(gps.canGetLocation() == true)
-            {
-                longitude = gps.getLongitude();
-                latitude = gps.getLatitude();
-
-                //Send text
-                smsManager.sendTextMessage(sPhone, null, sMessage, null, null);
-                Toast.makeText(gps.getApplicationContext(), "SMS Sent Successfully", Toast.LENGTH_LONG).show();
-            }
-        }
-        else {
-            //When edit text value is blank
-            //Display toast
-            Toast.makeText(gps.getApplicationContext(), "Enter Value First", Toast.LENGTH_SHORT).show();
-        }
-    }
-
 }
